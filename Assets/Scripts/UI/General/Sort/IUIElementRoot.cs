c6ed17ecbd981cc490651ece723cb70e41c5201c
gorbatchevartsem <gorbatchevartsem@gmail.com>
2026-01-18 22:16:23 +0300

    Added UI useful sorting and orientation

---
+++ Assets/Scripts/UI/General/Sort/IUIElementRoot.cs
@@ -0,0 +1,11 @@
+﻿namespace UI.General.Sort
+{
+    /// <summary>
+    /// Marker interface used to identify a logical root UI element.
+    /// Serves as a tag for grouping UI elements under a common root,
+    /// e.g. for analytics, tracking, or UI hierarchy identification.
+    /// </summary>
+    public interface IUIElementRoot
+    {
+    }
+}
---
+++ Assets/Scripts/UI/General/Sort/SortableUIUnit.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace UI.General.Sort
+{
+    public class SortableUIUnit : MonoBehaviour
+    {
+        [SerializeField]
+        private int sortOrder;
+
+        public int SortOrder
+        {
+            get => sortOrder;
+            set => sortOrder = value;
+        }
+    }
+}
